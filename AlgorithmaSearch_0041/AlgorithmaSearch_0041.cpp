f799a41ba6f605d12c1e0b4e4792846644ad2ef2
lilalr <gurennut@gmail.com>
2024-04-04 17:12:16 +0700

    membuat prosedur input

--- AlgorithmaSearch_0041/AlgorithmaSearch_0041.cpp
+++ AlgorithmaSearch_0041/AlgorithmaSearch_0041.cpp
@@ -5,3 +5,16 @@ int arr[20];    //array to be searched
 int n;          //number of elements in the array
 int i;          //index of array element
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the aray: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray shoud have minimum 1 and maximum 20 elements.\n\n";
+    }
+
+    
