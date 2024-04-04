58153a01f86bb2ecc0925386e08614a81da82d52
lilalr <gurennut@gmail.com>
2024-04-04 17:12:45 +0700

    menampilkan array

--- AlgorithmaSearch_0041/AlgorithmaSearch_0041.cpp
+++ AlgorithmaSearch_0041/AlgorithmaSearch_0041.cpp
@@ -17,4 +17,14 @@ void input()
             cout << "\nArray shoud have minimum 1 and maximum 20 elements.\n\n";
     }
 
-    
+    //accept array elements
+    cout << "\n-------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << "> ";
+        cin >> arr[i];
+    }
+}
+
